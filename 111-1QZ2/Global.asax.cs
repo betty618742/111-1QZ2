dc8697a3965179242dfffeddb97eb1727bc12bec
betty618742 <109111123@mail.aeust.edu.tw>
2022-10-25 15:39:09 +0800

    mm

--- 111-1QZ2/Global.asax.cs
+++ 111-1QZ2/Global.asax.cs
@@ -8,9 +8,14 @@ using System.Web.SessionState;
 namespace _111_1QZ2
 {
     public class Global : System.Web.HttpApplication
-    {
+    { 
         protected void Application_Start(object sender, EventArgs e)
+            lb_Msg.Text="Page_Load"
+    }
         {
+         protected void Application_Start(object sender, EventArgs e)
+            lb_Msg.Text="btn_Event"
+    }
         }
     }
 }
